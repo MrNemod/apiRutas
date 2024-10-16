73c84b16a9a48e06501aaa29db54630b48a5b5ec
MrNemod <a12680@universidad-une.com>
2024-10-16 11:22:28 -0600

    feta

---
+++ apiJWT.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import jwt
+import datetime
+
+app = Flask(__name__)
+
+SECRET_KEY = 'secret_key'
+
+@app.route('/generate_jwt', methods=['GET'])
+def generate_jwt():
+    payload = {
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+    return jsonify({'jwt': token})
+
+if __name__ == '__main__':
+    app.run(debug=True)
